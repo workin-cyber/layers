d7d0b285228b24b62d2900d4edd5a7c1c2224319
unknown <contact@workin.co.il>
2020-05-20 18:32:05 +0300

    static files

---
+++ build/precache-manifest.8265800c936430b1233e75914e410744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd9f6a5b12c3fca25b8b9bc7dd46617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "584726b9bed4cefaa209",
+    "url": "/static/css/main.607cbd8f.chunk.css"
+  },
+  {
+    "revision": "f4040c50585a80878bd0",
+    "url": "/static/js/2.d93411b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d93411b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584726b9bed4cefaa209",
+    "url": "/static/js/main.204b61f7.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
